a17aa9a293d7c765ddc57dd1fdfe19145d5e4f3e
Kosta45 <konstanlebed8@gmail.com>
2023-02-08 18:08:40 +0200

    Adding layout HTML/CSS and code JavaScript
    I adding  layout HTML/CSS and code JavaScript. Finished static layout HTML/CSS (no font-style). Ready function "Log in" - popup which a name and a surname.
    
    Я добавил разметку HTML/CSS и код JavaScript. Готовая статическая верстка HTML/CSS (без стиля шрифта). Готовая функция "Вход" - всплывающее окно с именем и фамилией.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+// Selectors
 const popupButton = document.querySelector('.popup-button');
+const menuCards = document.getElementById('menuCards');
 
-// Adding event listener
+// Adding Event Listener
 popupButton.addEventListener('click', closePopup)
+
+
+// Menu
+
+// const menuPizza = [
+//     { name: 'Carbonara', price: 30, button: "Add" },
+//     { name: 'Vegetable', price: 50, button: "Add" },
+//     { name: 'Royal', price: 30, button: "Add" },
+//     { name: 'Dragon', price: 60, button: "Add" },
+//     { name: 'Meat', price: 50, button: "Add" },
+//     { name: 'New', price: 60, button: "Add" },
+//     { name: 'Gribnaia', price: 40, button: "Add" },
+//     { name: 'Carbon', price: 33, button: "Add" },
+//     { name: 'Turtle', price: 28, button: "Add" }
+// ];
+
 
 
 
@@ -13,20 +31,19 @@ popupButton.addEventListener('click', closePopup)
 
 // Functions ================
 
+// function createMenuCard() {
+// }
+
 // Open field to type name and surname
-const openPopup = setTimeout((e) => {
+const openPopup = setTimeout(() => {
     const popupActive = document.querySelector('.popup');
     popupActive.classList.add('open');
-}, 2000);
+}, 1000);
 
 
 // CLose window with fields for type name and surname
-function closePopup(event) {
-    // event.preventDefault()
+function closePopup() {
     const popupActiveTwo = document.querySelector('.popup.open');
     popupActiveTwo.classList.add('close')
 };
-
-
-
 
